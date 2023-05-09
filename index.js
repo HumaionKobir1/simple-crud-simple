8e314ff1ed12b943d847dfa826fd44c057d26da9
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-09 13:10:48 +0600

    Added => update route

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ async function run() {
       res.send(result);
     })
 
+
+    app.get('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const user = await usersCollection.findOne(query)
+      res.send(user);
+    })
+
     app.post('/users', async(req, res) => {
       const user = req.body;
        console.log('new user', user);
