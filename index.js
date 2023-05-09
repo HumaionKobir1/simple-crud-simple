1975901ec8ee1ba0c2de4fbcc41110375551b59d
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-09 10:55:44 +0600

    added post and insert single user dta

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 app.use(cors());
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 
 // humaionkobir341
 // raGBpjfxqSpQbuLU
@@ -11,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://humaionkobir341:<password>@cluster0.4hywmoi.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://humaionkobir341:raGBpjfxqSpQbuLU@cluster0.4hywmoi.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,12 +27,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("usersDB");
+    const usersCollection = database.collection("users");
+
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+       console.log('new user', user);
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
