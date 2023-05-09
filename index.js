39ed74473705695876a14a31c3dffe6da84d82ae
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-05-09 11:18:45 +0600

    read data find

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ async function run() {
     const database = client.db("usersDB");
     const usersCollection = database.collection("users");
 
+    app.get('/users', async(req, res) => {
+      const cursor = usersCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res) => {
       const user = req.body;
        console.log('new user', user);
